c21a690677f6a9c648a5eb3857a27f52be31f526
Thomas-David Griedel <s73811@informatik.htw-dresden.de>
2018-06-14 21:31:35 +0200

    Removing Linux Artifacts

--- RUN_TUM.bat
+++ RUN_TUM.bat
@@ -1,2 +1,2 @@
-.\Examples\Monocular\Release\mono_tum.exe .\Vocabulary\ORBvoc.bin .\Examples\Monocular\TUM1.yaml C:\Users\David\Desktop\TUM1_Seq\
+.\Examples\Monocular\Release\mono_tum.exe .\data\ORBvoc.bin .\Examples\Monocular\TUM1.yaml C:\Users\David\Desktop\TUM1_Seq\
 ren KeyFrameTrajectory.txt TUMTrajectory.txt
